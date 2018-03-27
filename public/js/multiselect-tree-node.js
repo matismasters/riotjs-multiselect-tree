8e04799e263b061ed197cebd24e3e6b8cc175ca2
Matis Masters <me@matis.io>
2018-03-27 04:32:16 -0300

    Add onclick behaviour to leaves checkboxes
    Use our storage class to add/remove on check/uncheck events of
    the tree view.

--- public/js/multiselect-tree-node.js
+++ public/js/multiselect-tree-node.js
@@ -1,2 +1,12 @@
-riot.tag2('multiselect-tree-node', '<li> <input if="{leaf}" type="checkbox" name="{name}"> {data[parent.opts.name_attribute]} <ul if="{children.length > 0}"> <multiselect-tree-node each="{children}" name_attribute="{parent.opts.name_attribute}"></multiselect-tree-node> </ul> </li>', '', '', function(opts) {
+riot.tag2('multiselect-tree-node', '<li> <input if="{leaf}" type="checkbox" name="{name}" onclick="{selectNode}"> {data[parent.opts.name_attribute]} <ul if="{children.length > 0}"> <multiselect-tree-node each="{children}" name_attribute="{parent.opts.name_attribute}"></multiselect-tree-node> </ul> </li>', '', '', function(opts) {
+    this.storage = this.parent.storage;
+
+    this.id = Math.round(Math.random() * 100);
+    this.selectNode = function (e) {
+      if (e.target.checked) {
+        this.storage.addSelectedNode(e.item.name, e.item.data);
+      } else {
+        this.storage.removeSelectedNode(e.item.name)
+      }
+    }
 });
