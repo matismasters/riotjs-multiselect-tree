69e562113d61283c101573f31874c26ca61a400a
Matis Masters <me@matis.io>
2018-03-27 07:20:40 -0300

    Add final css for tree and use var for hasChildren

--- public/js/multiselect-tree-node.js
+++ public/js/multiselect-tree-node.js
@@ -1,14 +1,17 @@
-riot.tag2('multiselect-tree-node', '<li class="{selected: checked}"> <label for="{name}" onclick="{showHideChildren}"> <input id="{name}" if="{leaf}" type="checkbox" name="{name}" onclick="{selectNode}"> <span if="{children.length > 0}">{displayChildren ? \'-\' : \'+\'}</span> {data[parent.opts.name_attribute]} </label> <ul if="{children.length > 0 && displayChildren}"> <multiselect-tree-node each="{children}" name_attribute="{parent.opts.name_attribute}"></multiselect-tree-node> </ul> </li>', '', '', function(opts) {
+riot.tag2('multiselect-tree-node', '<li class="{selected: checked}"> <label riot-style="{indentationStyle}" for="{name}" onclick="{showHideChildren}"> <input id="{name}" if="{leaf}" type="checkbox" name="{name}" onclick="{selectNode}"> <span class="indentation-open-close-icon" if="{hasChildren}">{displayChildren ? \'-\' : \'+\'}</span> <span class="indentation-open-close-icon" if="{!hasChildren && !leaf}">&nbsp;</span> {data[parent.opts.name_attribute]} </label> <ul if="{children.length > 0 && displayChildren}"> <multiselect-tree-node each="{children}" name_attribute="{parent.opts.name_attribute}"></multiselect-tree-node> </ul> </li>', '', '', function(opts) {
+    this.displayChildren = true
     this.storage = this.parent.storage;
+    this.level = this.parent.level + 1;
+    this.hasChildren = this.children.length > 0;
+    this.indentationStyle = `padding-left: ${this.level * 5}px`;
 
-    this.id = Math.round(Math.random() * 100);
     this.selectNode = function (e) {
       if (e.target.checked) {
         this.checked = true;
         this.storage.addSelectedNode(e.item.name, e.item.data);
       } else {
         this.checked = false;
-        this.storage.removeSelectedNode(e.item.name)
+        this.storage.removeSelectedNode(e.item.name);
       }
     }
 
