c3311f08dcd6bb63bb23a400854e6e319d72b989
Matis Masters <me@matis.io>
2018-03-27 05:41:28 -0300

    Remove unnecessary test JS and removed CSS stylesheet

--- public/js/multiselect-tree-view.js
+++ public/js/multiselect-tree-view.js
@@ -1,11 +1,6 @@
-riot.tag2('multiselect-tree-view', '<button onclick="{updateAll}">update all</button> <ul class="root"> <multiselect-tree-node each="{rootChildren}" name_attribute="{parent.opts.name_attribute}"></multiselect-tree-node> </ul>', 'multiselect-tree-view ul li,[data-is="multiselect-tree-view"] ul li{ list-style: none; } multiselect-tree-view li.selected,[data-is="multiselect-tree-view"] li.selected{ color: blue } multiselect-tree-view li label,[data-is="multiselect-tree-view"] li label{ font-weight: normal; }', 'class="mtv-container"', function(opts) {
+riot.tag2('multiselect-tree-view', '<ul class="root"> <multiselect-tree-node each="{rootChildren}" name_attribute="{parent.opts.name_attribute}"></multiselect-tree-node> </ul>', 'multiselect-tree-view ul,[data-is="multiselect-tree-view"] ul{ padding: 0; } multiselect-tree-view li,[data-is="multiselect-tree-view"] li{ list-style: none; margin-bottom: -1px; display: block; border-top: 1px solid #eaebec; width: 100%; } multiselect-tree-view li.selected,[data-is="multiselect-tree-view"] li.selected{ border-left-width: 5px; border-left-color: #2b8be9; } multiselect-tree-view li label,[data-is="multiselect-tree-view"] li label{ font-weight: normal; white-space: normal; text-transform: none; font-size: 14px; line-height: 18px; padding: 14px; color: #555555; margin: 0; display: block; }', 'class="mtv-container"', function(opts) {
 
     this.rootChildren = sampleTreeHash.children;
-    this.bigFoot = { 'ana': 'never' };
 
     this.storage = new SelectedNodesStorage(this.root.localName);
-
-    this.updateAll = function () {
-      this.update();
-    }
 });
